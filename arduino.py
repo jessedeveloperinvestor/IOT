4e959ea8b6e2ae4abf2c5b009a03a735910eeba1
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-06-01 01:58:27 -0200

    Update arduino.py

--- arduino.py
+++ arduino.py
@@ -2,7 +2,8 @@
 import serial
 from serial.tools import list_ports
 for port in list_ports.comports():
-	print('Device: {} - port: {} '.format{port.description,port.device})
+	print('Device: '+port.description+' - port: '+port.device)
+	
 connection=serial.Serial('COM3',115200)
 action=input("Type in:\n'L' to turn on or\n'D' to turn off: ").upper()
 while action=='L' or action=='D':
