4519369ff8155b00c53c80779671529deb58e539
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-06-01 01:30:33 -0200

    Add files via upload

---
+++ arduino.py
@@ -0,0 +1,15 @@
+#pip install pyserial
+import serial
+from serial.tools import list_ports
+for port in list_ports.comports():
+	print('Device: {} - port: {} '.format{port.description,port.device})
+connection=serial.Serial('COM3',115200)
+action=input("Type in:\n'L' to turn on or\n'D' to turn off: ").upper()
+while action=='L' or action=='D':
+	if action=='L':
+		connection.write(b'1')
+	else:
+		connection.write(b'0')
+	action=input("Type in:\n'L' to turn on or\n'D' to turn off: ").upper()
+connection.close()
+print('Connection ended')
